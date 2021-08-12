5cd4a765b83c4bf2308ee6a9e4a02be775c4fcaa
adminsopra <deepak.kumar3@soprasteria.com>
2021-08-12 20:17:18 +0530

    first commit in App

--- src/index.js
+++ src/index.js
@@ -1,55 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
+import {createStore,combineReducers} from 'redux';//combine reducers in order to add multiple reducers in a single store
+import allReducers from "./Reducers/index"
+import {Provider} from "react-redux";
 
+const store=createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-
-const increment=()=>{
-  return {
-    type:"INCREMENT"
-  }
-}
-const decrement=()=>{
-  return {
-    type:"DECCREMENT"
-  }
-}
-
-
-
-
-
-
-const counter=(state=0,action)=>{
-  switch(action.type){
-    case "INCREMENT": 
-      return state+1
-
-    case "DECREMENT":
-      return state-1
-  }
-}
-let store=createStore(counter)
- 
-store.subscribe(()=>console.log(store.getState()));
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
 
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
