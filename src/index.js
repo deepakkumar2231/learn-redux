9f38814954c2370d77843402e016bfd63408fab4
adminsopra <deepak.kumar3@soprasteria.com>
2021-08-12 16:39:46 +0530

    first commit in App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+
+
+const increment=()=>{
+  return {
+    type:"INCREMENT"
+  }
+}
+const decrement=()=>{
+  return {
+    type:"DECCREMENT"
+  }
+}
+
+
+
+
+
+
+const counter=(state=0,action)=>{
+  switch(action.type){
+    case "INCREMENT": 
+      return state+1
+
+    case "DECREMENT":
+      return state-1
+  }
+}
+let store=createStore(counter)
+ 
+store.subscribe(()=>console.log(store.getState()));
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
 
 ReactDOM.render(
   <React.StrictMode>
